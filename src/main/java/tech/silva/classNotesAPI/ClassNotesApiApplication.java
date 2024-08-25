c7ae59967231e9f8b6d66f75ba267126ab6a6dd1
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-25 19:25:01 -0300

    feat: create project

---
+++ src/main/java/tech/silva/classNotesAPI/ClassNotesApiApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.classNotesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassNotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/silva/classNotesAPI/ClassNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.classNotesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
