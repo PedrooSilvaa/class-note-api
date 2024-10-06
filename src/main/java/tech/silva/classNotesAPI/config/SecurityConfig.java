7585981e0f8630b9a3573e169c636e9fb3c5d1d4
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-05 21:58:44 -0300

    feat: change cors config

--- src/main/java/tech/silva/classNotesAPI/config/SecurityConfig.java
+++ src/main/java/tech/silva/classNotesAPI/config/SecurityConfig.java
@@ -13,9 +13,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import tech.silva.classNotesAPI.jwt.JwtAuthorizationFilter;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableMethodSecurity
 @EnableWebSecurity
@@ -27,6 +32,8 @@ public class SecurityConfig {
 
         return http
                 .csrf(csrf -> csrf.disable())
+                // Habilita o CORS
+                .cors(withDefaults())
                 // Desabilita o formulário de login padrão do Spring Security
                 .formLogin(form -> form.disable())
                 // Desabilita a autenticação básica HTTP
@@ -46,12 +53,23 @@ public class SecurityConfig {
                 .build();
     }
 
+    // Configuração CORS global para permitir requisições da origem frontend
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // Adicione sua origem aqui
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -62,5 +80,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
 }
