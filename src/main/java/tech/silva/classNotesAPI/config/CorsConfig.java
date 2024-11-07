bafb2f698eb1151eb2bdb60e32a2425a81acb613
Pedro Henrique <125162325+PedrooSilvaa@users.noreply.github.com>
2024-11-06 21:21:27 -0300

    Atualizar o CorsConfig.java

--- src/main/java/tech/silva/classNotesAPI/config/CorsConfig.java
+++ src/main/java/tech/silva/classNotesAPI/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
+
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
